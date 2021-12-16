a1288147ce0e94ded9dbd9fb012e78432c6f3fdd
Marcello <marcellorocha632@gmail.com>
2021-12-15 22:45:50 -0300

    Add a function to clock

--- main.js
+++ main.js
@@ -10,51 +10,55 @@ const displayDate = document.querySelector('.date').innerText = `${day} / ${mont
 const displayWeekDay = document.querySelector('.week-day');
 const displayClock = document.querySelector('.clock-field');
 
-switch (weekDay) {
-    case 0:
-        weekDay = 'Sunday';
-        break;
-    case 1:
-        weekDay = 'Monday';
-        break;
-    case 2:
-        weekDay = 'Thuesday';
-        break;
-    case 3:
-        weekDay = 'Wednesday';
-        break;
-    case 4:
-        weekDay = 'Thursday';
-        break;
-    case 5:
-        weekDay = 'Friday';
-        break;
-    case 6:
-        weekDay = 'Saturday';
-        break;
-}
-
-displayWeekDay.innerText = weekDay;
-
-setInterval(function () {
-    seconds++;
-
-    if (seconds > 59) {
-        seconds = 0;
-        minutes++;
-
-        if (minutes > 59) {
-            minutes = 0;
-            hours++;
-
-            if (hours > 23) {
-                hours = 0;
+function clock() {
+    switch (weekDay) {
+        case 0:
+            weekDay = 'Sunday';
+            break;
+        case 1:
+            weekDay = 'Monday';
+            break;
+        case 2:
+            weekDay = 'Thuesday';
+            break;
+        case 3:
+            weekDay = 'Wednesday';
+            break;
+        case 4:
+            weekDay = 'Thursday';
+            break;
+        case 5:
+            weekDay = 'Friday';
+            break;
+        case 6:
+            weekDay = 'Saturday';
+            break;
+    }
+    
+    displayWeekDay.innerText = weekDay;
+    
+    setInterval(function () {
+        seconds++;
+    
+        if (seconds > 59) {
+            seconds = 0;
+            minutes++;
+    
+            if (minutes > 59) {
+                minutes = 0;
+                hours++;
+    
+                if (hours > 23) {
+                    hours = 0;
+                }
             }
         }
-    }
+    
+        let formatClock = `${hours < 10 ? '0' + hours : hours} : ${minutes < 10 ? '0' + minutes : minutes} : ${seconds < 10 ? '0' + seconds : seconds}`;
+    
+        displayClock.innerText = formatClock;
+    
+    }, 1000);
+}
 
-    let formatClock = `${hours < 10 ? '0' + hours : hours} : ${minutes < 10 ? '0' + minutes : minutes} : ${seconds < 10 ? '0' + seconds : seconds}`;
-
-    displayClock.innerText = formatClock;
-
-}, 1000);
+clock();
